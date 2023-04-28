9af44eeda5565ce32647e5a35b4a1deb7db9130d
Swartanaputra <130826999+Swartanaputra@users.noreply.github.com>
2023-04-28 22:17:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function openPopup1() {
+    document.getElementById("myPopup1").style.display = "block";
+  }
+  
+  function closePopup1() {
+    document.getElementById("myPopup1").style.display = "none";
+  }
+  
+  function openPopup2() {
+    document.getElementById("myPopup2").style.display = "block";
+  }
+  
+  function closePopup2() {
+    document.getElementById("myPopup2").style.display = "none";
+  }
+  
+  function openPopup3() {
+    document.getElementById("myPopup3").style.display = "block";
+  }
+  
+  function closePopup3() {
+    document.getElementById("myPopup3").style.display = "none";
+  }
+
+  
